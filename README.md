fca37713680554b8df05ef2dc637e936d258a951
Phillip Lange~ <langephil@ecs.vuw.ac.nz>
2019-04-02 13:31:11 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+#Awhina Git Workshop
+
+##Setting Up Git
+	1. GOTO: https://git-scm.com/
+	2. Install for your operating system
+	3. Setup Git Bash if on windows
+	4. GOTO: https://github.com/ (You are probably there if you are reading this
+	5. Create an account
+###In terminal/Git Bash:
+	6. git config user.name "FIRST_NAME LAST_NAME"
+	7. git config user.email "MY_NAME@example.com"
+
+
+
+##Clone the Repository
---
+++ src/Factorials.java
@@ -0,0 +1,37 @@
+/**
+ * Awhina Git workshop Excercise
+ * @author langephil
+ *
+ */
+public class Factorials {
+
+	public static  long iterFactorial(int n) {
+	    long fact = 1;
+	    for (int i = 2; i <= n; i++) {
+	        fact = fact * i;
+	    }
+	    return fact;
+	}
+
+	public static  long recursiveFactorial(int n) {
+		//FIXME: Your implementation here
+		return 0;
+	}
+
+	public static  long streamFactorial(int n) {
+		//FIXME: Your implementation here
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int a;
+		if(args.length == 1) {
+			a = Integer.parseInt(args[0]);
+		}
+		else a = 10;
+		System.out.println(a+"! = " + iterFactorial(a));
+	}
+
+
+
+}
